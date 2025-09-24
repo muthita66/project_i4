9dff19b627e92238a3e9ea2117a58de7d72beefe
muthita66 <muthitap66@nu.ac.th>
2025-09-24 14:46:48 +0700

    Update

--- db.php
+++ db.php
@@ -1,14 +1,22 @@
 <?php
-$host = '172.31.38.87';
-$user = 'galleryDB';
-$dbname = 'art_gellery';
-$pass = '123456';
+$db_host = '172.31.41.54';   
+$db_name = 'webuser';        
+$db_user = 'gallery_db';     
+$db_pass = '123456';         
 
-$connect = new mysqli($host, $user, $pass, $dbname);
+$dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
 
-if (!$connect) {
-    die("Connection failed: ". mysqli_connect($connect));
-} else {
-    mysqli_set_charset($connect, 'utf8');
+// ตั้งค่า Options สำหรับ PDO
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // แจ้ง Error แบบ Exception
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // ดึงข้อมูลเป็น Associative Array
+    PDO::ATTR_EMULATE_PREPARES   => false,                  
+];
+
+// เชื่อมต่อ PDO
+try {
+    $pdo = new PDO($dsn, $db_user, $db_pass, $options);
+} catch (PDOException $e) {
+    die("<h1>Connection failed</h1><p>" . $e->getMessage() . "</p>");
 }
 ?>
